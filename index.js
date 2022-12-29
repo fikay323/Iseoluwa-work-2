d212849a88b51ba757502856118e5eaf3771a945
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2022-12-29 11:50:40 +0100

    done

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // copyToClipboard("Hello, world!");
+let copied = document.querySelector('.copied')
 
 async function copyToClipboard(text) {
   try {
     await navigator.clipboard.writeText(text);
     console.log("Text copied to clipboard");
+    copied.style.visibility = 'visible'
+    setTimeout(() =>{
+      copied.style.visibility = 'hidden'
+    }, 1000)
+
   } catch (error) {
     console.error("Failed to copy text: ", error);
   }
